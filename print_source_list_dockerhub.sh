6394707bfc71741f36ece3ae7e612656e677baac
youngjun0827 <youngjun0827@gmail.com>
2024-04-18 17:00:40 -0700

    fixed bug for print_source_list_dockerhub.sh

--- print_source_list_dockerhub.sh
+++ print_source_list_dockerhub.sh
@@ -9,7 +9,7 @@ while true; do
     for REPO in $REPOS;do
         for TAG in $(curl -s "https://hub.docker.com/v2/repositories/${ORG}/${REPO}/tags/" | jq -r '.results | .[].name' | sort);do
             SOURCE="docker://$ORG/$REPO:$TAG"
-            OSNAME=$(grep "PRETTY_NAME" /etc/os-release | sed -e 's/PRETTY_NAME=//g' -e 's/"//g' -e 's/ /-/g')
+            OSNAME=$(singularity exec $SOURCE grep "PRETTY_NAME" /etc/os-release | sed -e 's/PRETTY_NAME=//g' -e 's/"//g' -e 's/ /-/g')
             SOFTWARE=$(echo $(singularity exec $SOURCE cat /app/software.list) | sed 's/ /,/g')
             if [ -z "$SOFTWARE" ];then
                 SOFTWARE="Unknown"
