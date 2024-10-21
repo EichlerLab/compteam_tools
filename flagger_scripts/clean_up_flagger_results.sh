06d345712094144e9334ffe3cf8c57e6d4bd178b
Julie Wertz <jwertz1@uw.edu>
2024-10-21 10:28:27 -0700

    Rename final bed dir

--- flagger_scripts/clean_up_flagger_results.sh
+++ flagger_scripts/clean_up_flagger_results.sh
@@ -5,13 +5,13 @@
 # Run this from directory containing cromwell-executions.
 # ** Note ** This will delete whole cromwell-executions directories minus final bed
 
-mkdir -p final_beds_alt_removed
+mkdir -p final_beds
 
 for j in $(ls cromwell-executions/*FlaggerEndToEnd*); do
     outpath=$(find cromwell-executions/*FlaggerEndToEnd*/"$j" -path '*/output/*final.bed' -o -path '*/output/*flagger.no_Hap.bed' 2>/dev/null)
     if [ -f "$outpath" ]; then
         echo Removing cromwell-executions/*FlaggerEndToEnd*/"$j"
-        mv "$outpath" final_beds_alt_removed
+        mv "$outpath" final_beds
         rm -rf cromwell-executions/*FlaggerEndToEnd*/"$j"
     else
         echo Skipping "$j"- Missing final bed
