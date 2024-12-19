166ea98663c93d94f1e755230ecc6537ab2fd300
Julie Wertz <jwertz1@uw.edu>
2024-12-19 10:03:17 -0800

    Change to Flagger v1.x output format

--- flagger_scripts/clean_up_flagger_results.sh
+++ flagger_scripts/clean_up_flagger_results.sh
@@ -6,14 +6,28 @@
 # ** Note ** This will delete whole cromwell-executions directories minus final bed
 
 mkdir -p final_beds
-
+found_any_outfile=false
 for j in $(ls cromwell-executions/*FlaggerEndToEnd*); do
-    outpath=$(find cromwell-executions/*FlaggerEndToEnd*/"$j" -path '*/output/*final.bed' -o -path '*/output/*flagger.no_Hap.bed' 2>/dev/null)
-    if [ -f "$outpath" ]; then
+    found_outfile=false
+    # nohap outpath has the correct sample name in it, but prediction bed is desired output with all blocks including Hap- use this and rename
+    nohap_outpath=$(find cromwell-executions/*FlaggerEndToEnd*/"$j" -path '*/output/*flagger.no_Hap.bed')
+    full_outpath=$(find cromwell-executions/*FlaggerEndToEnd*/"$j" -path '*/output/*prediction.bed')
+    if [ -f "$nohap_outpath" ] && [ -f "$full_outpath" ]; then
+        mv "$full_outpath" final_beds/$(basename "$nohap_outpath")
+        found_outfile=true
+        found_any_outfile=true
+    fi
+
+    if $found_outfile; then
         echo Removing cromwell-executions/*FlaggerEndToEnd*/"$j"
-        mv "$outpath" final_beds
         rm -rf cromwell-executions/*FlaggerEndToEnd*/"$j"
     else
         echo Skipping "$j"- Missing final bed
     fi
 done
+
+if $found_any_outfile; then
+    echo "Wrote final beds to final_beds directory"
+else
+    echo "No output files found"
+fi
