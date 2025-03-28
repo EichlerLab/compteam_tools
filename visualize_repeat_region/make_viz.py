cd82402c3c191e14aa293102e198cd6d9e3f2068
Julie Wertz <jwertz1@uw.edu>
2025-03-28 16:12:47 -0700

    Expose additional parameters

--- visualize_repeat_region/make_viz.py
+++ visualize_repeat_region/make_viz.py
@@ -1,14 +1,16 @@
 # Make gene output html to be converted to image
 import sys
-from yattag import Doc
 import re
-from collections import defaultdict, namedtuple
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-import numpy as np
-from Bio import SeqIO
 import math
 import json
+from collections import defaultdict, namedtuple
+import argparse
+import numpy as np
+from pathlib import Path
+from Bio import SeqIO
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from yattag import Doc
 
 OutputLine = namedtuple('OutputLine', [
     'seq', 'num_repeats', 'num_haps', 'interrs_str', 'padding_i', 'seq_no_tags',
@@ -18,31 +20,21 @@ TrimmedSequence = namedtuple('TrimmedSequence', [
     'trimmed_seq', 'repeat_seq', 'seq_pre_repeat', 'seq_post_repeat'
 ])
 
-json_fname = sys.argv[1]
-with open(json_fname) as f:
-    j = json.load(f)
-
 REPEAT_UNIT_SHAPE = '●'
 MIN_HAPS_SHOW_BARS = 5
 MIN_SEQ_LEN = 5
 
-GENE_NAME = j.get('gene_name')
-ASM_SEQUENCES_FNAME = j.get('asm_sequences_fname')
-TRGT_SEQUENCES_FNAME = j.get('trgt_sequences_fname', None)
-SEX_PER_SAMPLE_FNAME = j.get('sex_per_sample_fname', None) # only include if on chrX
-SUBSTITUTIONS = j.get('substitutions')
-SPECIAL_EXPR_SUBSTITUTIONS = j.get('special_expr_substitutions')
-COLOR_PER_REPEAT_UNIT = j.get('color_per_repeat_unit')
-MAIN_REPEAT_UNIT = j.get('main_repeat_unit')
-INTERRUPTIONS_TO_COUNT = j.get('interruptions_to_count', [])
-BEGIN_SEQS = j.get('begin_seqs') # pre-repeat sequence, use first matching, trim before
-END_SEQS = j.get('end_seqs')  # post-repeat sequence, use first matching, trim after
-
 def main():
+    args = parse_args()
+    assert args.out.is_dir()
+    global config
+    with open(args.gene_json) as f:
+        config = json.load(f)
+
     male_samples = []
-    if SEX_PER_SAMPLE_FNAME is not None:
+    if config['sex_per_sample_fname'] is not None:
         sex_per_sample = {}
-        with open(SEX_PER_SAMPLE_FNAME) as f:
+        with open(config['sex_per_sample_fname']) as f:
             f.readline()
             for line in f:
                 line = line.strip().split('\t')
@@ -51,48 +43,50 @@ def main():
         male_samples = set([z for z in sex_per_sample if sex_per_sample[z] == 'Male'])
 
     print('--Reading in ASM sequences--')
-    asm_seq_per_hap = read_in_asm(ASM_SEQUENCES_FNAME)
+    asm_seq_per_hap = read_in_asm(config['asm_sequences_fname'])
     asm_haps_per_repeat_seq, asm_rep_seq_tuple_per_repeat_seq = get_haps_per_repeat_seq(asm_seq_per_hap, male_samples)
 
-    if TRGT_SEQUENCES_FNAME is not None:
+    if config['trgt_sequences_fname'] is not None:
         print('--Reading in TRGT sequences--')
-        trgt_seq_per_hap  = read_in_trgt(TRGT_SEQUENCES_FNAME)  # TRGT sequence in repeat region
+        trgt_seq_per_hap  = read_in_trgt(config['trgt_sequences_fname'])  # TRGT sequence in repeat region
         trgt_haps_per_repeat_seq, trgt_rep_seq_tuple_per_repeat_seq = get_haps_per_repeat_seq(trgt_seq_per_hap, male_samples)
 
     print('--Parsing inputs...--')
     print('--All sequences--')
     (sorted_out_lines, subs_used, asm_repeat_seqs) = process_sequences(
         asm_haps_per_repeat_seq.keys(), asm_haps_per_repeat_seq, asm_rep_seq_tuple_per_repeat_seq,
-        'hap'
+        sort_by=args.sort_by, min_haps=args.min_haps, min_repeats=args.min_repeats
     )
-    if TRGT_SEQUENCES_FNAME is not None:
+    if config['trgt_sequences_fname'] is not None:
         print('--TRGT only--')
         (sorted_out_lines_trgt, subs_used_trgt, trgt_repeat_seqs) = process_sequences(
-            trgt_haps_per_repeat_seq.keys(), trgt_haps_per_repeat_seq, trgt_rep_seq_tuple_per_repeat_seq, 'hap',
-            seqs_to_exclude=asm_repeat_seqs
+            trgt_haps_per_repeat_seq.keys(), trgt_haps_per_repeat_seq, trgt_rep_seq_tuple_per_repeat_seq,
+            seqs_to_exclude=asm_repeat_seqs, sort_by=args.sort_by, min_haps=args.min_haps, min_repeats=args.min_repeats
         )
         print('--ASM only--')
         (sorted_out_lines_asm, subs_used_asm, _) = process_sequences(
-            asm_haps_per_repeat_seq.keys(), asm_haps_per_repeat_seq, asm_rep_seq_tuple_per_repeat_seq, 'hap',
-            seqs_to_exclude=trgt_repeat_seqs
+            asm_haps_per_repeat_seq.keys(), asm_haps_per_repeat_seq, asm_rep_seq_tuple_per_repeat_seq,
+            seqs_to_exclude=trgt_repeat_seqs, sort_by=args.sort_by, min_haps=args.min_haps, min_repeats=args.min_repeats
         )
         #print(sorted(list(trgt_repeat_seqs), key=lambda u:len(u))[:10])
         print('--ASM + TRGT match--')
         (sorted_out_lines_match, subs_used_match, _) = process_sequences(
-            asm_haps_per_repeat_seq.keys(), asm_haps_per_repeat_seq, asm_rep_seq_tuple_per_repeat_seq, 'hap',
+            asm_haps_per_repeat_seq.keys(), asm_haps_per_repeat_seq, asm_rep_seq_tuple_per_repeat_seq,
             seqs_to_exclude=asm_repeat_seqs.symmetric_difference(trgt_repeat_seqs),  # in one set but not the other
-            trgt_haps=trgt_haps_per_repeat_seq
+            trgt_haps=trgt_haps_per_repeat_seq, sort_by=args.sort_by, min_haps=args.min_haps, min_repeats=args.min_repeats
         )
 
     print('--Writing HTMLs--')
-    write_html(f'{GENE_NAME}_viz.html', sorted_out_lines, subs_used)
-    if TRGT_SEQUENCES_FNAME is not None:
-        write_html(f'{GENE_NAME}_viz_asm_only.html', sorted_out_lines_asm, subs_used_asm)#, divide_into=2)
-        write_html(f'{GENE_NAME}_viz_TRGT_only.html', sorted_out_lines_trgt, subs_used_trgt)#, divide_into=3)
-        write_html(f'{GENE_NAME}_viz_matches_TRGT.html', sorted_out_lines_match, subs_used_match)#, divide_into=3)
+    if asm_repeat_seqs:
+        write_html(args.out / f'{config['gene_name']}_viz.html', sorted_out_lines, subs_used)
+    if config['trgt_sequences_fname'] is not None:
+        write_html(args.out / f'{config['gene_name']}_viz_TRGT_only.html', sorted_out_lines_trgt, subs_used_trgt)#, divide_into=3)
+        if asm_repeat_seqs:
+            write_html(args.out / f'{config['gene_name']}_viz_asm_only.html', sorted_out_lines_asm, subs_used_asm)#, divide_into=2)
+            write_html(args.out / f'{config['gene_name']}_viz_matches_TRGT.html', sorted_out_lines_match, subs_used_match)#, divide_into=3)
 
     print('--Statistics--')
-    if TRGT_SEQUENCES_FNAME is not None:
+    if config['trgt_sequences_fname'] is not None:
         n_asm_seqs_confirmed = 0
         n_asm_seqs_not_confirmed = 0
         n_single_hap_asm_seqs_confirmed = 0
@@ -113,6 +107,39 @@ def main():
         print(f'{n_asm_seqs_not_confirmed} assembly sequences NOT confirmed by TRGT')
         print(f'{n_single_hap_asm_seqs_confirmed} single-haplotype assembly sequences confirmed by TRGT')
         print(f'{n_single_hap_asm_seqs_not_confirmed} single-haplotype assembly sequences NOT confirmed by TRGT')
+
+
+def parse_args():
+    p = argparse.ArgumentParser(
+        description='Make a figure with repeat sequences encoding according to '
+        'specified string substitutions.'
+    )
+    p.add_argument(
+        'gene_json', type=Path, help='Path to JSON defining input filepaths and '
+        'substitutions for gene.'
+    )
+    p.add_argument(
+        '-s', '--sort_by', type=str, help='Sort by either descending repeat '
+        'length or number of haplotypes with repeat sequence. (hap)',
+        default='hap', choices=['repeat', 'hap']
+    )
+    p.add_argument(
+        '-m', '--min_haps', type=int, help='Only retain repeat sequences found '
+        'in at least this number of haplotypes. (0)', default=0
+    )
+    p.add_argument(
+        '-r', '--min_repeats', type=int, help='Only retain sequences with at '
+        'least this many repeat units. (0)', default=0
+    )
+    p.add_argument(
+        '-o', '--out', type=Path, help='Output directory for plot HTMLs. (.)',
+        default=Path('.')
+    )
+    if len(sys.argv) < 2:
+        p.print_help()
+        sys.exit(0)
+    return p.parse_args()
+
 
 def get_haps_per_repeat_seq(seq_per_hap, male_samples):
     haps_per_repeat_seq = defaultdict(set)  # repeat seq: samples (phased) it occurs in
@@ -140,7 +167,7 @@ def get_haps_per_repeat_seq(seq_per_hap, male_samples):
 
     for hap in seq_per_hap:
         sample = hap.split('_')[0]
-        # males w >1 haplotype on chr X - not real. a LOT of these for TRGT
+        # males w >1 haplotype on chr X
         if not (
             ((hap.endswith('_hap2')) and (sample in males_two_haps)) or
             (sample in males_two_different_haps)
@@ -162,7 +189,7 @@ def read_in_asm(asm_sequences_fname):
             hap = hap_obj.name.split('[')[0]
             assert hap not in seq_per_hap
             # trim to exact region for output
-            trimmed_seq = trim_sequence(str(hap_obj.seq), BEGIN_SEQS, END_SEQS, hap)
+            trimmed_seq = trim_sequence(str(hap_obj.seq), config['begin_seqs'], config['end_seqs'], hap)
             if trimmed_seq is not None:
                 seq_per_hap[hap] = trimmed_seq
     return seq_per_hap
@@ -175,11 +202,12 @@ def read_in_trgt(trgt_sequences_fname):
         f.readline()
         for i, line in enumerate(f):
             line = line.strip()
+            #print(line)
             sample, seq = line.split()
             hap = sample + ('_hap1' if (i % 2 == 0) else '_hap2')
             assert hap not in seq_per_hap
             # trim to exact region for output
-            trimmed_seq = trim_sequence(seq, BEGIN_SEQS, END_SEQS, hap)
+            trimmed_seq = trim_sequence(seq, config['begin_seqs'], config['end_seqs'], hap)
             # **Note these haplotypes don't necessarily equate to ASM _hap1 and _hap2
             if trimmed_seq is not None:
                 seq_per_hap[hap] = trimmed_seq
@@ -188,15 +216,15 @@ def read_in_trgt(trgt_sequences_fname):
 
 def process_sequences(
     unique_seqs, haps_per_repeat_seq, rep_seq_tuple_per_repeat_seq,
-    sort_by='hap', min_haps=1, seqs_to_exclude=None, trgt_haps=None,
+    sort_by='hap', min_haps=0, min_repeats=0, seqs_to_exclude=None, trgt_haps=None,
     noncanonical_interrs_only=False
 ):
-    assert sort_by in ['hap', 'repeats']
+    assert sort_by in ['hap', 'repeat']
     out_lines = []
     interruptions_d_overall = defaultdict(int)  # total interruptions incl multiple per hap (to print)
     interruptions_d_overall_num_haps = defaultdict(int) # num haplotypes that HAVE interruption (to print)
     repeats_hist = defaultdict(int)
-    subs_used = set([MAIN_REPEAT_UNIT])
+    subs_used = set([config['main_repeat_unit']])
     n_have_noncanonical_interrs = 0
 
     repeat_seqs = set()  # does include seqs in seqs_to_exclude
@@ -215,7 +243,7 @@ def process_sequences(
 
         (trimmed_seq, repeat_seq, seq_pre_repeat, seq_post_repeat) = rep_seq_tuple_per_repeat_seq[repeat_seq]
 
-        interr_region = repeat_seq[repeat_seq.index(MAIN_REPEAT_UNIT):repeat_seq.rindex(MAIN_REPEAT_UNIT) + len(MAIN_REPEAT_UNIT)] # actually interrupting repeats
+        interr_region = repeat_seq[repeat_seq.index(config['main_repeat_unit']):repeat_seq.rindex(config['main_repeat_unit']) + len(config['main_repeat_unit'])] # actually interrupting repeats
         repeat_seqs.add(repeat_seq)
         if (seqs_to_exclude) and (repeat_seq in seqs_to_exclude):
             continue
@@ -224,14 +252,14 @@ def process_sequences(
         out_seq_repeat_patterns_maintain_num_chars = repeat_seq
         interruptions_d = defaultdict(int)
         interrs_w_ind = []
-        for sub in SUBSTITUTIONS:
-            expr = SPECIAL_EXPR_SUBSTITUTIONS[sub] if (sub in SPECIAL_EXPR_SUBSTITUTIONS) else sub 
-            curr_matches = [(z.start(), sub) for z in re.finditer(expr, out_seq_repeat_patterns_maintain_num_chars) if sub != MAIN_REPEAT_UNIT]
+        for sub in config['substitutions']:
+            expr = config['special_expr_substitutions'][sub] if (sub in config['special_expr_substitutions']) else sub 
+            curr_matches = [(z.start(), sub) for z in re.finditer(expr, out_seq_repeat_patterns_maintain_num_chars) if sub != config['main_repeat_unit']]
             interrs_w_ind += curr_matches
             if curr_matches:
                 subs_used.add(sub)
             out_seq_repeat_patterns = re.sub(expr,
-                f'<span style="color: {COLOR_PER_REPEAT_UNIT[sub]};">{REPEAT_UNIT_SHAPE}</span>',
+                f'<span style="color: {config['color_per_repeat_unit'][sub]};">{REPEAT_UNIT_SHAPE}</span>',
                 out_seq_repeat_patterns
             )
             out_seq_repeat_patterns_no_tags = re.sub(expr, REPEAT_UNIT_SHAPE, out_seq_repeat_patterns_no_tags)
@@ -249,14 +277,16 @@ def process_sequences(
         if not interrs_w_ind:
             interruptions_d_overall_num_haps[None] += num_haps
 
-        has_noncanonical_interrs = any([z[1] not in INTERRUPTIONS_TO_COUNT for z in set(interrs_w_ind)])
+        has_noncanonical_interrs = any([z[1] not in config['interruptions_to_count'] for z in set(interrs_w_ind)])
         if has_noncanonical_interrs:
             n_have_noncanonical_interrs += 1
         if noncanonical_interrs_only and (not has_noncanonical_interrs):
             continue
         out_seq = seq_pre_repeat + out_seq_repeat_patterns + seq_post_repeat
         out_seq_no_tags = seq_pre_repeat + out_seq_repeat_patterns_no_tags + seq_post_repeat
-        num_repeats = interr_region.count(MAIN_REPEAT_UNIT) + sum([interr_region.count(z) for z in INTERRUPTIONS_TO_COUNT])
+        num_repeats = interr_region.count(config['main_repeat_unit']) + sum([interr_region.count(z) for z in config['interruptions_to_count']])
+        if num_repeats < min_repeats:
+            continue
         repeats_hist[num_repeats] += num_haps
 
         out_line = OutputLine(
@@ -270,7 +300,7 @@ def process_sequences(
         )
         out_lines.append(out_line)
 
-    unused_subs = [z for z in SUBSTITUTIONS if z not in subs_used]
+    unused_subs = [z for z in config['substitutions'] if z not in subs_used]
     if unused_subs:
         print('Unused encodings: ' + ', '.join(unused_subs))
     print(dict(sorted(repeats_hist.items())))
@@ -338,9 +368,10 @@ def write_html_divided(
     out_fname_final = out_fname.replace('.html', f'_part{part_num}.html') if (part_num > 1) else out_fname
     write_html(out_fname_final, out_lines[out_index[-1]:], subs_used, max_haps=max_haps_overall)
 
+
 def write_html(out_fname, out_lines, subs_used, write_legend=True, max_haps=0):
     if write_legend:
-        out_legend = out_fname.replace('.html', '_legend.html')
+        out_legend = str(out_fname).replace('.html', '_legend.html')
     max_seq_len = 0
     max_trgt_haps = 0
     for l in out_lines:
@@ -354,7 +385,7 @@ def write_html(out_fname, out_lines, subs_used, write_legend=True, max_haps=0):
     with tag('html'):
         with tag('head'):
             with tag('title'):
-                text(f'{GENE_NAME} Sequences')
+                text(f'{config['gene_name']} Sequences')
             with tag('style', type='text/css'):
                 text('body { font-family: "DejaVu Sans Mono", monospace; }')
                 text('th, td { padding-right: 8px; text-align: left; }')
@@ -415,16 +446,16 @@ def write_html(out_fname, out_lines, subs_used, write_legend=True, max_haps=0):
         with tag('html'):
             with tag('head'):
                 with tag('title'):
-                    text(f'{GENE_NAME} Legend')
+                    text(f'{config['gene_name']} Legend')
                 with tag('style', type='text/css'):
                     text('body { font-family: "DejaVu Sans Mono", monospace; }')
                     text('th, td { padding-right: 8px; text-align: left; }')
 
             with tag('body'):
                 with tag('table'):
-                    legend_first = [MAIN_REPEAT_UNIT] + INTERRUPTIONS_TO_COUNT
-                    for seq in legend_first + [z for z in COLOR_PER_REPEAT_UNIT if (z not in legend_first and z in subs_used)]:
-                        color = COLOR_PER_REPEAT_UNIT[seq]
+                    legend_first = [config['main_repeat_unit']] + config['interruptions_to_count']
+                    for seq in legend_first + [z for z in config['color_per_repeat_unit'] if (z not in legend_first and z in subs_used)]:
+                        color = config['color_per_repeat_unit'][seq]
                         with tag('tr'):
                             with tag('td'):
                                 text(f'{seq}')
